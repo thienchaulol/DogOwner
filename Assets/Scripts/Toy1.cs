87440dade6f4c7933ac871feea8945fe04d69e13
thienchaulol <tchau006@ucr.edu>
2016-09-29 13:57:32 -0700

    Created Squeeky Ball Toy
    Finished code for Squeeky Ball toy. Fully functional. May need to lower
    receive rates.
    Received Squeeky Ball in 154 Taps (1% rate per tap)

--- Assets/Scripts/Toy1.cs
+++ Assets/Scripts/Toy1.cs
@@ -4,33 +4,46 @@ using System.Collections;
 public class Toy1 : MonoBehaviour {
 
 	public string toyName = "Squeeky Ball";
-	public float toyChance = 0.095f;
-	public float treatIncreaseFromToy = 1.1f;
+	public float toyChance = 0.01f;
+	public float treatIncreaseFromToy = 2f;
 
 	public DogTreats player;
 	public UnityEngine.UI.Text disp;
 
-	bool received = false;
+	public bool received = false;
 	bool didTap = false;
+	int taps = 0;
 	
 	// Update is called once per frame
 	void Update () {
-		disp.text = "Toy: " + toyName + "\n" + "Receive chance per tap: " + toyChance + "%" 
-			+ "\n" + "Increases all treat accumulation by: " + treatIncreaseFromToy + "%" + "\n";
+		if (!received) {
+			disp.text = "Toy: " + toyName + "\n" + "Receive chance per tap: " + toyChance * 100f + "%"
+			+ "\n" + "Increases treat per tap by: " + treatIncreaseFromToy * 100f + "%" + "\n";
+		} else {
+			disp.text = "Toy: " + toyName + "\n" + "You found the toy!!"
+				+ "\n" + "Increases treat per tap by: " + treatIncreaseFromToy * 100f + "%" + "\n";
+		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
+			taps += 1;
 			didTap = true;
-			received = true;
 		}
-		if (didTap == true) {
+	}
+
+	void FixedUpdate(){
+		if (didTap && !received) {
 			//random chance to receive toy
-
+			int num = Random.Range(0,100);
+			int receiveNumber = 69;
+			Debug.Log (num);
+			if (num == receiveNumber) {
+				Debug.Log (taps);
+				received = true;
+			}
 		}
+		if (didTap && received) {
+			player.totalTreats += Mathf.Round ((player.treatsPerTap * treatIncreaseFromToy) - player.treatsPerTap);
+		}
+		didTap = false;
 	}
 
-	public void Tapped(){
-		if (received) {
-			player.treatsPerTap *= treatIncreaseFromToy;
-			player.playerTreatsPerSec *= treatIncreaseFromToy;
-		}
-	}
 }
