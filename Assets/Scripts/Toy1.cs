a4c9bd871b9a1d86e3aad83f223ef8a7742d1595
thienchaulol <tchau006@ucr.edu>
2016-09-30 13:11:21 -0700

    toy notification
    made toy nofitication

--- Assets/Scripts/Toy1.cs
+++ Assets/Scripts/Toy1.cs
@@ -9,19 +9,21 @@ public class Toy1 : MonoBehaviour {
 
 	public DogTreats player;
 	public UnityEngine.UI.Text disp;
+	public UnityEngine.UI.Text toyNotif;
 
 	public bool received = false;
 	bool didTap = false;
+	bool show = false;
 	int taps = 0;
 	
 	// Update is called once per frame
 	void Update () {
 		if (!received) {
-			disp.text = "Toy: " + toyName + "\n" + "Receive chance per tap: " + toyChance * 100f + "%"
-			+ "\n" + "Increases treat per tap by: " + treatIncreaseFromToy * 100f + "%" + "\n";
+			disp.text = "Toy: " + toyName + "\n" + "Receive chance every tap: " + toyChance * 100f + "%"
+			+ "\n" + "Increases all treat tap/accumlation by: " + treatIncreaseFromToy * 100f + "%" + "\n";
 		} else {
 			disp.text = "Toy: " + toyName + "\n" + "You found the toy!!"
-				+ "\n" + "Increases treat per tap by: " + treatIncreaseFromToy * 100f + "%" + "\n";
+				+ "\n" + "Increases all treat tap/accumulation by: " + treatIncreaseFromToy * 100f + "%" + "\n";
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			taps += 1;
@@ -32,16 +34,28 @@ public class Toy1 : MonoBehaviour {
 	void FixedUpdate(){
 		if (didTap && !received) {
 			//random chance to receive toy
-			int num = Random.Range(0,500);
+			int num = Random.Range(0,100);
 			int receiveNumber = 69;
 			//Debug.Log (num);
 			if (num == receiveNumber) {
 				Debug.Log (taps);
 				received = true;
+				show = true;
+				StartCoroutine (toyNotifFunc());
+				show = false;
 				player.treatsMultiplier = treatIncreaseFromToy;
 			}
 		}
 		didTap = false;
+	}
+
+	IEnumerator toyNotifFunc() {
+		//Debug.Log("Before Waiting 2 seconds");
+		if(show)
+		toyNotif.text = "You received the " + toyName + " in " + taps + " taps!!";
+		yield return new WaitForSeconds(10);
+		toyNotif.text = "";
+		//Debug.Log("After Waiting 2 Seconds");
 	}
 
 }
