d626827ef6fa1b1b5eac6993d26dbf66c3043e9c
thienchaulol <tchau006@ucr.edu>
2016-09-30 13:34:41 -0700

    made chewy toy
    editted toy script to make it abstract Kappa

--- Assets/Scripts/Toy1.cs
+++ Assets/Scripts/Toy1.cs
@@ -6,15 +6,20 @@ public class Toy1 : MonoBehaviour {
 	public string toyName = "Squeeky Ball";
 	public float toyChance = 0.01f;
 	public float treatIncreaseFromToy = 2f;
+	public float maxRange = 0f;
+	public float receiveNumber;
 
 	public DogTreats player;
 	public UnityEngine.UI.Text disp;
 	public UnityEngine.UI.Text toyNotif;
 
 	public bool received = false;
+	public bool show = false;
 	bool didTap = false;
-	bool show = false;
 	int taps = 0;
+
+	public bool testReceive = false;
+	float num;
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,7 +27,7 @@ public class Toy1 : MonoBehaviour {
 			disp.text = "Toy: " + toyName + "\n" + "Receive chance every tap: " + toyChance * 100f + "%"
 			+ "\n" + "Increases all treat tap/accumlation by: " + treatIncreaseFromToy * 100f + "%" + "\n";
 		} else {
-			disp.text = "Toy: " + toyName + "\n" + "You found the toy!!"
+			disp.text = "Toy: " + toyName + "\n" + "Received after " + taps + " taps."
 				+ "\n" + "Increases all treat tap/accumulation by: " + treatIncreaseFromToy * 100f + "%" + "\n";
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
@@ -32,10 +37,10 @@ public class Toy1 : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
+		num = Random.Range(0f,maxRange);
+		receiveNumber = 69f;
 		if (didTap && !received) {
 			//random chance to receive toy
-			int num = Random.Range(0,100);
-			int receiveNumber = 69;
 			//Debug.Log (num);
 			if (num == receiveNumber) {
 				Debug.Log (taps);
@@ -46,13 +51,15 @@ public class Toy1 : MonoBehaviour {
 				player.treatsMultiplier = treatIncreaseFromToy;
 			}
 		}
+		if (testReceive)
+			StartCoroutine (toyNotifFunc ());
 		didTap = false;
 	}
 
 	IEnumerator toyNotifFunc() {
 		//Debug.Log("Before Waiting 2 seconds");
 		if(show)
-		toyNotif.text = "You received the " + toyName + " in " + taps + " taps!!";
+		toyNotif.text = "You've discovered: " + toyName + " in " + taps + " taps.";
 		yield return new WaitForSeconds(10);
 		toyNotif.text = "";
 		//Debug.Log("After Waiting 2 Seconds");
