95c61e1890a6c14e3bc8e9ce8303179dad0d0308
Thien Chau <tchau006@ucr.edu>
2016-11-08 05:24:32 -0800

    aura script
    still trying to fix aura script. aura script is a weird name.. position
    1 still prematurely resets..or position 2 and 3  reset late.

--- Assets/Scripts/Dog Related Scripts/DogSpriteAura.cs
+++ Assets/Scripts/Dog Related Scripts/DogSpriteAura.cs
@@ -6,32 +6,32 @@ public class DogSpriteAura : MonoBehaviour {
 
 	public Dog1 player;	//player object
 	public Sprite aura1;	//sprite object
-	//want to display treat aura on dog: tiny random treats appearing around the dog while purchased
-	//									 follow dog object when dog jumps
 	public float spriteSpeed;	//up speed
 	float initialSpriteSpeed;	//initial up speed
 	IList<Vector2> positions = new List<Vector2>();	//list to hold possible positions of sprite aura
 	Vector2 p1;	//position 1
 	Vector2 p2;	//position 2
 	Vector2 p3;	//position 3
-	float initialY;	//initial Y of game object
+	float initialP1Y;	//initial Y position of game object
+	float initialP2Y;
+	float initialP3Y;
+	int positionNum;
 
 	void Start(){
 		initialSpriteSpeed = spriteSpeed;	//store initial sprite speed for upgrades
+		positionNum = 0;
 
 		p1 = transform.position;	//calc p1
+		initialP1Y = p1.y;
 		positions.Add(p1);	//store p1
 
 		p2 = new Vector2(p1.x - 0.25f, p1.y - 0.25f);	//calc p2
+		initialP1Y = p2.y;
 		positions.Add(p2);	//store p2
 
 		p3 = new Vector2 (p1.x + 0.25f, p1.y - 0.25f);	//calc p3
+		initialP1Y = p3.y;
 		positions.Add(p3);	//store p3
-
-		//choose and set random position
-		transform.position = positions[Random.Range(0, positions.Count)];
-		//set initial y to the initial random position
-		initialY = transform.position.y;
 	}
 
 	// Update is called once per frame
@@ -62,9 +62,32 @@ public class DogSpriteAura : MonoBehaviour {
 		//move sprite up
 		transform.Translate (Vector2.up * spriteSpeed * Time.deltaTime);	//sprite movement
 		//check if it's past the ceiling value
-		if (transform.position.y > initialY + 0.5f) {
-			//	if so, reset the position to positions 1, 2, or 3
-			transform.position = positions[Random.Range(0, positions.Count)];
+		if (positionNum == 0) {
+			if (transform.position.y > initialP1Y + 0.5f) {
+				resetPosition ();
+			}
+		}
+		if (positionNum == 1) {
+			if (transform.position.y > initialP2Y + 0.5f) {
+				resetPosition ();
+			}
+		}
+		if (positionNum == 2) {
+			if (transform.position.y > initialP3Y + 0.5f) {
+				resetPosition ();
+			}
+		}
+	}
+
+	void resetPosition(){
+		//	if so, reset the position to positions 1, 2, or 3
+		//transform.position = positions[Random.Range(0, positions.Count)];
+		if (positionNum < 2) {
+			transform.position = positions [positionNum];
+			positionNum++;
+		} else {
+			transform.position = positions [positionNum];
+			positionNum = 0;
 		}
 	}
 }
