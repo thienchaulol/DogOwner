ea18267241946d3c3e59331a191fdd015d56aa1f
thienchaulol <tchau006@ucr.edu>
2016-11-08 08:29:23 -0800

    Editted DogAura Script
    DogAura script now works as intended.

--- Assets/Scripts/Dog Related Scripts/DogAura.cs
+++ Assets/Scripts/Dog Related Scripts/DogAura.cs
@@ -2,36 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class DogSpriteAura : MonoBehaviour {
+public class DogAura : MonoBehaviour {
 
 	public Dog1 player;	//player object
 	public Sprite aura1;	//sprite object
 	public float spriteSpeed;	//up speed
+	public float auraHeight; //how high aura goes before resetting
 	float initialSpriteSpeed;	//initial up speed
-	IList<Vector2> positions = new List<Vector2>();	//list to hold possible positions of sprite aura
-	Vector2 p1;	//position 1
-	Vector2 p2;	//position 2
-	Vector2 p3;	//position 3
-	float initialP1Y;	//initial Y position of game object
-	float initialP2Y;
-	float initialP3Y;
-	int positionNum;
+	IList<Vector2> positions = new List<Vector2>();	//list to hold possible positions of aura
+	Vector2 p0, p1, p2;	//positions 1, 2, and 3
+	float initialP0Y, initialP1Y, initialP2Y;	//initial positions
+	int currPosition;	//current position
 
 	void Start(){
 		initialSpriteSpeed = spriteSpeed;	//store initial sprite speed for upgrades
-		positionNum = 0;
+		currPosition = 0;
 
-		p1 = transform.position;	//calc p1
+		p0 = transform.position;	//calc p1
+		initialP0Y = p0.y;
+		positions.Add(p0);	//store p1
+
+		p1 = new Vector2(p0.x - 0.25f, p0.y - 0.25f);	//calc p2
 		initialP1Y = p1.y;
-		positions.Add(p1);	//store p1
+		positions.Add(p1);	//store p2
 
-		p2 = new Vector2(p1.x - 0.25f, p1.y - 0.25f);	//calc p2
-		initialP1Y = p2.y;
-		positions.Add(p2);	//store p2
-
-		p3 = new Vector2 (p1.x + 0.25f, p1.y - 0.25f);	//calc p3
-		initialP1Y = p3.y;
-		positions.Add(p3);	//store p3
+		p2 = new Vector2 (p0.x + 0.25f, p0.y - 0.25f);	//calc p3
+		initialP2Y = p2.y;
+		positions.Add(p2);	//store p3
 	}
 
 	// Update is called once per frame
@@ -62,18 +59,18 @@ public class DogSpriteAura : MonoBehaviour {
 		//move sprite up
 		transform.Translate (Vector2.up * spriteSpeed * Time.deltaTime);	//sprite movement
 		//check if it's past the ceiling value
-		if (positionNum == 0) {
-			if (transform.position.y > initialP1Y + 0.5f) {
+		if (currPosition == 0) {
+			if (transform.position.y > (initialP0Y + auraHeight)) {
 				resetPosition ();
 			}
 		}
-		if (positionNum == 1) {
-			if (transform.position.y > initialP2Y + 0.5f) {
+		if (currPosition == 1) {
+			if (transform.position.y > (initialP1Y + auraHeight)) {
 				resetPosition ();
 			}
 		}
-		if (positionNum == 2) {
-			if (transform.position.y > initialP3Y + 0.5f) {
+		if (currPosition == 2) {
+			if (transform.position.y > (initialP2Y + auraHeight)) {
 				resetPosition ();
 			}
 		}
@@ -82,12 +79,12 @@ public class DogSpriteAura : MonoBehaviour {
 	void resetPosition(){
 		//	if so, reset the position to positions 1, 2, or 3
 		//transform.position = positions[Random.Range(0, positions.Count)];
-		if (positionNum < 2) {
-			transform.position = positions [positionNum];
-			positionNum++;
+		if (currPosition < 2) {
+			currPosition++;
+			transform.position = positions [currPosition];
 		} else {
-			transform.position = positions [positionNum];
-			positionNum = 0;
+			currPosition = 0;
+			transform.position = positions [currPosition];
 		}
 	}
 }
