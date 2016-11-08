9e549476fbc85a959c3a5b7e1b532005b1ae32dc
thienchaulol <tchau006@ucr.edu>
2016-11-07 22:08:19 -0800

    editted dog aura script
    editted dog aura script

--- Assets/Scripts/Dog Related Scripts/DogSpriteAura.cs
+++ Assets/Scripts/Dog Related Scripts/DogSpriteAura.cs
@@ -14,26 +14,22 @@ public class DogSpriteAura : MonoBehaviour {
 	Vector2 p1;	//position 1
 	Vector2 p2;	//position 2
 	Vector2 p3;	//position 3
-	float initP1Y;
-	float initP2Y;
-	float initP3Y;
-	int randNum;	//stores random position number
+	int initialY;	//initial Y of game object
 
 	void Start(){
-		randNum = Random.Range (0, positions.Count);	//first random number
 		initialSpriteSpeed = spriteSpeed;	//store initial sprite speed for upgrades
 
 		p1 = transform.position;	//calc p1
-		initP1Y = p1.y;	//store initial p1.y for reset check
 		positions.Add(p1);	//store p1
 
 		p2 = new Vector2(p1.x - 0.25f, p1.y - 0.25f);	//calc p2
-		initP2Y = p2.y;	//store initial p2.y for reset check
 		positions.Add(p2);	//store p2
 
 		p3 = new Vector2 (p1.x + 0.25f, p1.y - 0.25f);	//calc p3
-		initP3Y = p3.y;	//store initial p3.y for reset check
 		positions.Add(p3);	//store p3
+
+		//choose random position
+		//set initial y to the random position
 	}
 
 	// Update is called once per frame
@@ -60,21 +56,15 @@ public class DogSpriteAura : MonoBehaviour {
 	}
 
 	void Movement(){	//deals with movement of game object
+		//pseudocode
+		//move sprite up
 		transform.Translate (Vector2.up * spriteSpeed * Time.deltaTime);	//sprite movement
-		if(this.gameObject.transform.position.y >= initP1Y + .5f && randNum == 1){
-			randNum = Random.Range (0, positions.Count);	//new random number
-			transform.position = positions[randNum];	//choose new position from positions list
-			p1.y = initP1Y;
+		//check if it's past the ceiling value
+		if(gameObject.transform.position.y > initialY + 0.25f){
+			//	if so, reset the value and change it's position
+
 		}
-		if(this.gameObject.transform.position.y >= initP2Y + .5f && randNum == 2){
-			randNum = Random.Range (0, positions.Count);	//new random number
-			transform.position = positions[randNum];	//choose new position from positions list
-			p2.y = initP2Y;
-		}
-		if(this.gameObject.transform.position.y >= initP3Y + .5f && randNum == 3){
-			randNum = Random.Range (0, positions.Count);	//new random number
-			transform.position = positions[randNum];	//choose new position from positions list
-			p3.y = initP3Y;
-		}
+
+		transform.Translate (Vector2.up * spriteSpeed * Time.deltaTime);	//sprite movement
 	}
 }
