0f449ba3b8a55dabd661e94c04450644475373ca
Thien Chau <tchau006@ucr.edu>
2016-11-28 22:49:10 -0800

    Temporary Cloud Sprite, Info Tab Changes
    - Added a temporary cloud sprite
    - Changed color of Info Tab to a color that's easier on the eyes

--- Assets/Scripts/Treat Related Scripts/DogTreatsMultiplier.cs
+++ Assets/Scripts/Treat Related Scripts/DogTreatsMultiplier.cs
@@ -19,7 +19,7 @@ public class DogTreatsMultiplier : MonoBehaviour {
 			costToUpgrade = player.treatsPerTap * 15f;
 		}
 		newTreatsPerTap = Mathf.Round(player.treatsPerTap * 2.15f);	//treats per tap algorithm
-		dispUpgradeCost.text = "Tap Upgrade: " + player.dispValToNiceString(costToUpgrade) + "\n";	//upgrade cost display
+		dispUpgradeCost.text = "Upgrade Tap: \n" + player.dispValToNiceString(costToUpgrade) + "\n";	//upgrade cost display
 		//dispNewTreatsPerTap.text = "New Treats Per Tap: " + 
 		//	player.dispValToNiceString(newTreatsPerTap * player.treatsMultiplier); //new treats cost display
 	}
