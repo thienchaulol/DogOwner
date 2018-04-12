3937bcd800e46df30b88649c3705f28d5f4eb7d1
Thien Chau <tchau006@ucr.edu>
2018-04-12 11:33:43 -0700

    Menu
    Added Game Menu that transitions to the main Game Scene

---
+++ Assets/Scripts/Game_manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void play_button()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+
+    public void instructions_button()
+    {
+        SceneManager.LoadScene("Instructions", LoadSceneMode.Single);
+    }
+}
